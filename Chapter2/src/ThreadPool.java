80fac6b19cf921f3110f4b188b958de4e3e81e88
Sercan Külcü <sercankulcu@gmail.com>
2023-02-03 20:36:20 +0300

    .

---
+++ Chapter2/src/ThreadPool.java
@@ -0,0 +1,38 @@
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+/*
+ * Here's a basic Java code that demonstrates thread pooling:
+ * 
+ * In this code, we have created a fixed size thread pool of 5 threads using the 
+ * Executors.newFixedThreadPool method. We then submit 10 tasks to the executor service, 
+ * which are executed by the available threads in the pool. When all tasks are completed, 
+ * we shut down the executor service to release its resources.
+ * 
+ * */
+
+public class ThreadPool {
+    static class Task implements Runnable {
+        private int taskNumber;
+        public Task(int taskNumber) {
+            this.taskNumber = taskNumber;
+        }
+        public void run() {
+            System.out.println("Task " + taskNumber + " is running");
+            try {
+                Thread.sleep(10000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+    public static void main(String[] args) {
+        ExecutorService executorService = Executors.newFixedThreadPool(5);
+        for (int i = 0; i < 10; i++) {
+            Task task = new Task(i);
+            executorService.execute(task);
+        }
+        executorService.shutdown();
+    }
+}
---
+++ Chapter6/src/SpinLock.java
@@ -0,0 +1,54 @@
+
+import java.util.concurrent.atomic.AtomicReference;
+
+/*
+ * Here's a basic Java code that demonstrates spin lock in an OS context using AtomicReference:
+ * 
+ * In this code, we have used AtomicReference to store the owner of the lock. The lock is 
+ * acquired by a task when compareAndSet sets the owner to the current task. The lock is released 
+ * by setting the owner to null. This approach ensures that the lock is acquired and released in 
+ * a thread-safe manner, and the lock is acquired by the task that first calls compareAndSet.
+ * 
+ * */
+
+public class SpinLock {
+    static class Lock {
+        private AtomicReference<Thread> owner = new AtomicReference<>();
+        public void lock() {
+            Thread currentThread = Thread.currentThread();
+            while (!owner.compareAndSet(null, currentThread)) { }
+        }
+        public void unlock() {
+            owner.set(null);
+        }
+    }
+    static class Task implements Runnable {
+        private Lock lock;
+        public Task(Lock lock) {
+            this.lock = lock;
+        }
+        public void run() {
+            lock.lock();
+            System.out.println(Thread.currentThread().getName() + " acquired lock");
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } finally {
+                lock.unlock();
+                System.out.println(Thread.currentThread().getName() + " released lock");
+            }
+        }
+    }
+    public static void main(String[] args) {
+        Lock lock = new Lock();
+        Task task1 = new Task(lock);
+        Task task2 = new Task(lock);
+        Task task3 = new Task(lock);
+        Task task4 = new Task(lock);
+        new Thread(task1, "Task 1").start();
+        new Thread(task2, "Task 2").start();
+        new Thread(task3, "Task 3").start();
+        new Thread(task4, "Task 4").start();
+    }
+}
