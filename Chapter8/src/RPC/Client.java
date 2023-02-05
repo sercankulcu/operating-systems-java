ab15093638ca38159ef3aff68ee16dc149822dd8
Sercan Külcü <sercankulcu@gmail.com>
2023-02-05 10:48:42 +0300

    .

---
+++ Chapter8/src/RPC/Client.java
@@ -0,0 +1,16 @@
+package RPC;
+
+//Client.java
+import java.rmi.Naming;
+
+public class Client {
+
+	public static void main(String[] args) {
+		try {
+			RemoteInterface stub = (RemoteInterface) Naming.lookup("//localhost/MyServer");
+			System.out.println("Addition: " + stub.add(3, 4));
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
---
+++ Chapter8/src/RPC/RemoteInterface.java
@@ -0,0 +1,9 @@
+package RPC;
+
+//RemoteInterface.java
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+
+public interface RemoteInterface extends Remote {
+ int add(int a, int b) throws RemoteException;
+}
